19fa688ddfddff7b45e0a75fe96e7d23b62c9326
CarlosOstorga480 <87096795+CarlosOstorga480@users.noreply.github.com>
2026-08-20 18:37:40 -0600

    Add database connection script for Azure SQL

---
+++ conexion.php
@@ -0,0 +1,18 @@
+<?php
+
+$serverName = "sql-tilin.database.windows.net";
+
+$connectionOptions = array(
+    "Database" => "BD-BENITO",
+    "Uid" => "adminsql",
+    "PWD" => "mVqivAdd1234",
+    "CharacterSet" => "UTF-8"
+);
+
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+if ($conn === false) {
+    die("Error de conexión con Azure SQL:<br><pre>" . print_r(sqlsrv_errors(), true) . "</pre>");
+}
+
+?>
